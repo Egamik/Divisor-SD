45bc8903a8c021ccea94915a42a45de7096ddb6e
Gmk <44400533+Egamik@users.noreply.github.com>
2020-12-03 10:03:17 -0300

    Update registrador_sh.vhd

--- registrador_sh.vhd
+++ registrador_sh.vhd
@@ -15,7 +15,6 @@ END registrador_sh;
 ARCHITECTURE behav OF registrador_sh IS
 signal fio: std_logic_vector(n-1 downto 0);
 BEGIN
-	Q <= fio;
 	process(clk)
 	begin
 		if (clk'event and clk = '1') then
@@ -24,6 +23,7 @@ BEGIN
 			elsif (carga = '0' and shift = '1') then
 				out_shift <= fio(fio'high);
 				fio <= fio(fio'high-1 downto fio'low) & in_shift;
+				Q <= fio;
 			end if;
 		end if;
 	end process;
